88ae22824e9a7ec7a26843aa0c9a4754c64eefa4
EH J <50188104+haileyport@users.noreply.github.com>
2022-09-09 15:40:41 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,14 @@
+function solution(n, m) {
+    var answer = [];
+    const gcd = (a, b) => {
+        if(b === 0) return a;
+        return gcd(b, a%b);
+    };
+    const lcm = (a, b) => {
+        return (a*b) / gcd(a, b);
+    };
+    
+    answer.push(gcd(n,m));
+    answer.push(lcm(n,m));
+    return answer;
+}
