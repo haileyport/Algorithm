eba72b0016080a08c41376163dd9ef48f9766525
EH J <50188104+haileyport@users.noreply.github.com>
2022-09-12 10:44:52 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,9 +1,10 @@
 function solution(a, b) {
-    let answer = 0;
     // 내적 = 각 i번째 요소끼리 곱해서 더하는 거
     
-    a.forEach((el, idx) => {
-        answer += el * b[idx]
-    })
-    return answer;
+    // a.forEach((el, idx) => {
+    //     answer += el * b[idx]
+    // })
+    return a.reduce((acc, cur, idx) => {
+        return acc += cur * b[idx];
+    }, 0)
 }
