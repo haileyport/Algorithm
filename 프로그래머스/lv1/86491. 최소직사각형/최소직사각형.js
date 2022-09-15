19ac85079bd968d90e5e5a7338b7bc4ffa1e4b1a
haileyport <sugar9935@gmail.com>
2022-09-15 21:30:09 +0900

    최소직사각형 수정

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,15 +1,25 @@
 // 1점
 function solution(sizes) {
-    sizes = sizes.map(el => {
-        return el.sort((a,b) => a-b)
+  sizes = sizes
+    .map((el) => {
+      return el.sort((a, b) => a - b);
     })
-    
-    const wallet = [0, 0];
-    
-    sizes.forEach(el => {
-        wallet[0] = el[0] > wallet[0] ? el[0] : wallet[0]
-        wallet[1] = el[1] > wallet[1] ? el[1] : wallet[1]
-    })
-    
-    return wallet[0] * wallet[1]
+    .sort((a, b) => {
+      if (a[0] > b[0]) {
+        return 1;
+      } else if (a[0] < b[0]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+  const wallet = [0, 0];
+
+  sizes.forEach((el) => {
+    wallet[0] = el[0] > wallet[0] ? el[0] : wallet[0];
+    wallet[1] = el[1] > wallet[1] ? el[1] : wallet[1];
+  });
+
+  return wallet[0] * wallet[1];
 }
