38469014095a1c460abb7fb1239a01bb97e92ddf
EH J <50188104+haileyport@users.noreply.github.com>
2022-09-26 07:02:31 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,20 +1,7 @@
 function solution(new_id) {
-    const one = new_id.toLowerCase();
-    const two = one.replace(/[^a-z 0-9 \-\_\.]+/g, "");
-    const three = two.replace(/\.{1,}/g, ".");
-    const four = three.replace(/^\./, "");
-    let five = '';
+    const one = new_id.toLowerCase().replace(/[^\w-_.]/g, "").replace(/\.+/g, ".").replace(/^\.|\.$/g, "").replace(/^$/, "a").replace(/\.$/, "").slice(0, 15).replace(/\.$/, '');
     
-    if (four == '') five = "a";
-    else five = four;
-          
-    const six = five.substr(0,15).replace(/\.$/, "");
-    let seven = six;
+    const len = one.length;
     
-    while(seven.length<3){
-        const repeat = seven.substr(seven.length-1);
-        seven=seven+repeat
-    }
-    
-    return seven;
+    return len > 2 ? one : one + one.charAt(len-1).repeat(3 - len);
 }
