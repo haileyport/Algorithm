9836e04269daa3946eab91f25f57099b1ad16895
EH J <50188104+haileyport@users.noreply.github.com>
2022-10-04 16:27:33 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.15 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,19 +1,18 @@
 function solution(s) {
     let trans = s;
     let origin = s;
-    let zero = 0;
-    let turn = 0;
+    const ans = [0, 0]
     
     while(trans.length > 1) {        
-        turn++;
+        ans[0]++;
         
         trans = trans.replace(/0/g, "");
         const len = trans.length;
-        zero += origin.length - len;
+        ans[1] += origin.length - len;
         
         trans = len.toString(2);
         origin = trans;
     }
     
-    return [turn, zero];
+    return ans;
 }
